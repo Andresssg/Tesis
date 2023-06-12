1ed2a6da62a67b2184aa9d3f8ce5a7b8b58115e4
Andresssg <andres-guevara1@upc.edu.co>
2023-06-12 01:33:26 -0500

    Add canva to draw line in first frame and send points to back

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,17 +1,47 @@
-import { useState } from 'react'
+import { useContext, useState } from 'react'
 import Loading from './components/Loading'
 import Canva from './components/Canva'
+import { RequestContext } from './contexts/RequestContext'
 
 function App () {
   const [isLoading, setIsLoading] = useState(true)
   const [showImage, setShowImage] = useState(false)
-  const [image, setImage] = useState('https://estaticos.elcolombiano.com/binrepository/720x674/0c54/720d565/none/11101/PCKJ/wilson-perro-pastor-belga-ninos-indigenas-rescatados_42572597_20230609220651.jpg')
+  const [parkName, setParkName] = useState('')
+  const [video, setVideo] = useState('')
+  const [image, setImage] = useState('')
+  const { BASE_URL } = useContext(RequestContext)
+  const [data, setData] = useState()
 
   const handleUpload = (e) => {
     e.preventDefault()
-    window.alert('Hola')
+    const form = new FormData(e.target)
+    const formParkName = form.get('parkname')
+    const formVideo = form.get('video')
+    if (!formParkName || !formVideo.size) return window.alert('Por favor complete los campos')
+    getData(form)
     setShowImage(true)
   }
+
+  const getData = async (form) => {
+    const res = await fetch(`${BASE_URL}/upload/`, {
+      method: 'POST',
+      headers: {
+        // 'Content-Type': 'application/json'
+      },
+      body: form
+    })
+    const data = await res?.json()
+    if (!res.ok) {
+      return data?.error
+        ? window.alert(data?.error)
+        : window.alert(`Error al enviar los datos: ${res.statusText}`)
+    }
+    const { first_frame } = data
+    setImage(first_frame)
+    setData(data)
+    setIsLoading(false)
+  }
+
   return (
     <article>
       <section className='flex flex-col justify-center items-center w-full p-10 gap-5 text-4xl text-center text-gray-50'>
@@ -19,20 +49,26 @@ function App () {
         <h2 className='text-2xl md:w-4/12'>Sistema de carga de videos de parques para conteo de ingreso y salida de personas con IA</h2>
       </section>
       <section className='flex flex-col items-center justify-center w-full'>
-        <form onSubmit={handleUpload} className='flex flex-col p-5 gap-5 w-full md:w-2/6 '>
+        <form
+          onSubmit={handleUpload} className='flex flex-col p-5 gap-5 w-full md:w-4/6 lg:w-2/6 '
+        >
           <input
             type='text' name='parkname' id='parkname'
             placeholder='Ingrese el nombre del parque' className='p-2 focus:outline-2 focus:outline-sky-500'
+            defaultValue={parkName} onChange={(e) => setParkName(e.target.value)}
           />
-          <input type='file' name='video' id='video' accept='video/*' className='text-gray-300 cursor-pointer' />
-          <input type='submit' value='Subir video' className='p-2 bg-sky-500 cursor-pointer hover:bg-sky-300' />
+          <input
+            type='file' name='video' id='video' accept='video/*' className='text-gray-300 cursor-pointer'
+            defaultValue={video} onChange={(e) => setVideo(e.target.value)}
+          />
+          <input type='submit' value='Subir video' className='p-2 bg-sky-500 hover:bg-sky-300 cursor-pointer' />
         </form>
       </section>
       {showImage &&
         <section className='flex flex-col items-center justify-center w-full'>
           {isLoading
             ? <Loading />
-            : <Canva image={image} />}
+            : <Canva imageBase64={image} processedData={data} />}
         </section>}
     </article>
   )
--- Frontend/src/components/Canva.jsx
+++ Frontend/src/components/Canva.jsx
@@ -1,27 +1,160 @@
-function Canva ({ image }) {
-  const handleDraw = () => {
-    window.alert('Pintar linea')
+import React, { useContext, useEffect, useState } from 'react'
+import { Stage, Layer, Image, Circle, Line, Text } from 'react-konva'
+import { RequestContext } from '../contexts/RequestContext'
+
+function Canva ({ imageBase64, processedData }) {
+  const { BASE_URL } = useContext(RequestContext)
+  const [image, setImage] = useState(null)
+  const [imageSize, setImageSize] = useState({ width: 0, height: 0 })
+  const [lines, setLines] = useState([])
+
+  const [points, setPoints] = useState([
+    { x: 50, y: 100, color: 'green' }, // punto de inicio
+    { x: 100, y: 100, color: 'red' } // punto final
+  ])
+
+  // Calcular los límites teniendo en cuenta el radio del círculo
+  const radius = 10
+  const minX = 0
+  const minY = 0
+  const maxX = imageSize.width
+  const maxY = imageSize.height
+
+  const handleDragEnd = (index, e) => {
+    const newPoints = [...points]
+    const { x, y } = e.target.attrs
+
+    const newX = Number.isNaN(x) ? 0 : x
+    const newY = Number.isNaN(y) ? 0 : y
+
+    newPoints[index] = {
+      x: Math.max(minX, Math.min(maxX, newX)),
+      y: Math.max(minY, Math.min(maxY, newY)),
+      color: newPoints[index].color
+    }
+    setPoints(newPoints)
   }
 
-  const handleAnalyze = () => {
-    window.alert('Analizar video')
+  useEffect(() => {
+    if (imageBase64) {
+      fetch(imageBase64)
+        .then((response) => response.blob())
+        .then((blob) => {
+          const img = new window.Image()
+          img.onload = () => {
+            setImage(img)
+            setImageSize({ width: img.width / 2, height: img.height / 2 })
+          }
+          img.src = URL.createObjectURL(blob)
+        })
+    }
+  }, [imageBase64])
+
+  useEffect(() => {
+    const startPoint = points[0]
+    const endPoint = points[1]
+    const newLine = { start: startPoint, end: endPoint }
+    setLines([newLine])
+  }, [points])
+
+  const handleAnalyze = async () => {
+    const { x: startX, y: startY } = points[0]
+    const { x: endX, y: endY } = points[1]
+    const start = [startX * 2, startY * 2]
+    const end = [endX * 2, endY * 2]
+
+    const { park_name, video_name } = processedData
+    const payload = {
+      park_name,
+      video_name,
+      start,
+      end
+    }
+
+    const res = await fetch(`${BASE_URL}/detect/`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(payload)
+    })
+    const data = await res?.json()
+    if (!res.ok) return window.alert(data?.error || 'Hubo un problema al analizar el video')
+    console.log(data)
   }
+
   return (
-    <div className='flex flex-col p-5 gap-5 w-full md:w-2/6 text-gray-50'>
-      <h2 className='text-2xl uppercase font-medium'>Dibujar linea</h2>
-      <details>
+    <div className='flex flex-col p-5 gap-5 text-gray-50 w-full md:w-2/6 justify-center items-center '>
+      <h2 className='text-2xl uppercase font-medium'>Dibujar línea</h2>
+      <details className=' self-baseline'>
         <summary>
-          ¿Que hacer?
+          ¿Qué hacer?
         </summary>
-        <ol className='list-decimal px-6'>
-          <li>Primero haga click aqui para x vaina</li>
-          <li>Segundo haga no se que</li>
-          <li>Tercero disfrute</li>
+        <p className='font-bold'>Hay dos puntos uno que es el <span className='text-green-500'>INICIO</span> y el segundo que es el <span className='text-red-500'>FINAL</span> </p>
+        <ol className='list-decimal px-4'>
+          <li>Para mover un punto mantenga el click presionado sobre él y arrastrelo.</li>
+          <li>Para soltar el punto deje de mantener el click.</li>
+          <li>Una vez tenga los puntos definidos y vea la linea en la posición deseada, haga click en Analizar el video</li>
         </ol>
       </details>
-      <img src={image} />
+      {image && (
+        <Stage width={imageSize.width} height={imageSize.height}>
+          <Layer>
+            <Image
+              image={image}
+              width={imageSize.width}
+              height={imageSize.height}
+            />
+            {points.map((point, index) => (
+              <Circle
+                key={index}
+                x={point.x}
+                y={point.y}
+                radius={radius}
+                fill={point.color}
+                draggable
+                onDragEnd={(e) => handleDragEnd(index, e)}
+                dragBoundFunc={(pos) => {
+                  const newX = Math.max(minX, Math.min(maxX, pos.x))
+                  const newY = Math.max(minY, Math.min(maxY, pos.y))
+                  return { x: newX, y: newY }
+                }}
+              />
+            ))}
+            {lines.map((line, index) => {
+              const shouldInvert = line.end.x < line.start.x // Verificar la posición de los puntos
+              return (
+                <React.Fragment key={index}>
+                  <Line
+                    points={[line.start.x, line.start.y, line.end.x, line.end.y]}
+                    stroke='white'
+                    strokeWidth={4}
+                  />
+                  <Text
+                    x={(line.start.x + line.end.x) / 2}
+                    y={(line.start.y + line.end.y) / 2 - 10}
+                    text={shouldInvert ? 'Out' : 'In'} // Invertir el texto si es necesario
+                    fontSize={16}
+                    fontFamily='Arial'
+                    fill='black'
+                    align='center'
+                  />
+                  <Text
+                    x={(line.start.x + line.end.x) / 2}
+                    y={(line.start.y + line.end.y) / 2 + 10}
+                    text={shouldInvert ? 'In' : 'Out'} // Invertir el texto si es necesario
+                    fontSize={16}
+                    fontFamily='Arial'
+                    fill='black'
+                    align='center'
+                  />
+                </React.Fragment>
+              )
+            })}
+          </Layer>
+        </Stage>
+      )}
       <div className='flex w-full justify-evenly items-center font-medium'>
-        <button onClick={handleDraw} className='p-2 bg-sky-500 cursor-pointer hover:bg-sky-300'>Pintar linea</button>
         <button onClick={handleAnalyze} className='p-2 bg-sky-500 cursor-pointer hover:bg-sky-300'>Analizar video</button>
       </div>
     </div>
