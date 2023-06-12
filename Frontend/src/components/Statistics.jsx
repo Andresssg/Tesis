f8d6568534471441cb08e8e68ea0f5b34c42860f
Andresssg <andres-guevara1@upc.edu.co>
2023-06-12 14:02:04 -0500

    Create statistics and add statistics state to context

---
+++ Frontend/src/components/Statistics.jsx
@@ -0,0 +1,21 @@
+import { useContext } from 'react'
+import { RequestContext } from '../contexts/RequestContext'
+
+function Statistics () {
+  const { statistics } = useContext(RequestContext)
+  console.log(statistics)
+  const handleDownload = () => {
+    window.alert('descargar')
+  }
+  return (
+    <div className='text-gray-50 font-medium text-2xl'>
+      <h2>Conteo ingreso: {statistics.conteo.ingreso_personas}</h2>
+      <h2>Conteo Salida: {statistics.conteo.salida_personas}</h2>
+      <div className='flex w-full justify-evenly items-center font-medium text-base'>
+        <button onClick={handleDownload} className='p-2 bg-sky-500 cursor-pointer hover:bg-sky-300'>Descargar video</button>
+      </div>
+    </div>
+  )
+}
+
+export default Statistics
--- Frontend/src/contexts/RequestContext.jsx
+++ Frontend/src/contexts/RequestContext.jsx
@@ -1,12 +1,13 @@
-import { createContext } from 'react'
+import { createContext, useState } from 'react'
 
 export const RequestContext = createContext({})
 
 export function RequestContextProvider ({ children }) {
   const BASE_URL = 'http://localhost:8000'
+  const [statistics, setStatistics] = useState()
 
   return (
-    <RequestContext.Provider value={{ BASE_URL }}>
+    <RequestContext.Provider value={{ BASE_URL, statistics, setStatistics }}>
       {children}
     </RequestContext.Provider>
   )
