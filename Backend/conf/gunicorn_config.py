c3343fcac059f9564cca2d938be452dc5ee55869
Andresssg <andres-guevara1@upc.edu.co>
2023-06-27 18:42:04 -0500

    Add errorlog to gunicorn config

--- Backend/conf/gunicorn_config.py
+++ Backend/conf/gunicorn_config.py
@@ -5,3 +5,4 @@ pythonpath = '/home/tesisuser/Tesis/Backend/tesis_project'
 bind = '127.0.0.1:8000'
 workers = multiprocessing.cpu_count() * 2 + 1
 timeout = 1000
+errorlog = '/var/log/gunicorn/backendtesis.log'
