1bb9385ee5752e72ac20a07db9fb4f101c72b9aa
Andresssg <andres-guevara1@upc.edu.co>
2023-07-15 20:01:25 -0500

    Make default text to handle missing parks info and change parks json file

--- Frontend/src/components/ParkItem.jsx
+++ Frontend/src/components/ParkItem.jsx
@@ -1,6 +1,14 @@
 import parkIcon from '../assets/trees-icon.webp'
 
 function ParkItem ({ park = {}, setName }) {
+  const { name, code, scale, locality, address } = park
+  const defaultTexts = {
+    codeText: 'Sin código',
+    nameText: 'Sin nombre',
+    scaleText: 'Sin tipo',
+    addressText: 'Sin dirección',
+    localityText: 'Sin localidad'
+  }
   return (
     <div
       className='flex items-center gap-x-4 rounded-lg border-2 border-slate-800 border-opacity-20 p-2 w-full
@@ -9,10 +17,10 @@ function ParkItem ({ park = {}, setName }) {
     >
       <img src={parkIcon} alt='' className='h-14 p-2 rounded-full border-2 bg-slate-800' />
       <div>
-        <p className=''>Código: <span className='font-semibold'>{park.code}</span></p>
-        <p className='text-base'>Nombre: <span className='font-normal'>{park.name}</span></p>
-        <p className=''>Dirección: <span className='font-semibold'>{park.code}</span></p>
-        <p className=''>Tipo: <span className='font-semibold'>{park.code}</span></p>
+        <p>Código: <span className='font-semibold'>{code || defaultTexts.codeText}</span></p>
+        <p className='text-base'>Nombre: <span className='font-normal'>{name || defaultTexts.nameText}</span></p>
+        <p>Dirección: <span className='font-semibold'>{address || defaultTexts.addressText}</span> - Localidad: <span className='font-semibold'>{locality || defaultTexts.localityText}</span></p>
+        <p>Tipo: <span className='font-semibold'>{scale || defaultTexts.scaleText}</span></p>
       </div>
     </div>
   )
